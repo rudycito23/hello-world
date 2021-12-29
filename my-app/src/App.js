c5344d6d327d6c6c9a3c9722af6f9898b86b7c30
Rofo <88415585+rudycito23@users.noreply.github.com>
2021-12-29 16:58:20 -0600

    Samurai Jack updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,17 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Samurai Jack
+        </h1>
+        <img src="https://static.wikia.nocookie.net/samuraijack/images/5/5c/Samurai_Jack.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Congrats! This is your first website introducing Samurai Jack!
+        </p>
+        <p>
+          Click on the link below to learn more about Jack.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/Samurai_Jack"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Samurai Jack!
         </a>
       </header>
     </div>
